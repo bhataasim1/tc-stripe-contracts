2248a97da58c7346001a63c10ae81c215a7626d4
bhataasim1 <bhataasim700@gmail.com>
2026-04-09 12:00:44 +0530

    feat: export additional Stripe response types for API integration support

--- src/contracts/stipe/stripe.ts
+++ src/contracts/stipe/stripe.ts
@@ -10,3 +10,17 @@ export type StripeSubscriptionCancelParams = Stripe.SubscriptionCancelParams;
 export type StripeInvoice = Stripe.Invoice;
 export type StripeBillingPortalConfiguration = Stripe.BillingPortal.Configuration;
 export type StripePaymentIntent = Stripe.PaymentIntent;
+
+
+// Responses
+export type StripeAccountResponse = Stripe.Response<Stripe.Account>;
+export type StripeAccountLinkResponse = Stripe.Response<Stripe.AccountLink>;
+export type StripeBalanceResponse = Stripe.Response<Stripe.Balance>
+export type StripePriceResponse = Stripe.Response<Stripe.Price>
+export type StripeCustomerResponse = Stripe.Response<Stripe.Customer>
+export type StripeDeletedAccountResponse = Stripe.Response<Stripe.DeletedAccount>
+export type StripeCheckoutSessionResponse = Stripe.Response<Stripe.Checkout.Session>
+export type StripeBillingPortalSessionResponse = Stripe.Response<Stripe.BillingPortal.Session>
+export type StripePaymentIntentResponse = Stripe.Response<Stripe.PaymentIntent>;
+export type StripeProductResponse = Stripe.Response<Stripe.Product>
+export type StripeSubscriptionResponse = Stripe.Response<Stripe.Subscription>
